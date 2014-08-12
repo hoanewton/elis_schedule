f80f5f1cb0ceeaeef3614ab5322246cfea227edd
hoanewton <caohoa86hp@gmail.com>
2014-08-11 20:59:31 -0400

    days scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812005426) do
+ActiveRecord::Schema.define(version: 20140812005748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "days", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.datetime "start_time"
